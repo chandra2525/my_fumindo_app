fac3604bcea91b968770c24919197aa6c226c528
Chandra Tirtayansyah <chandratiryansyah@gmail.com>
2024-12-05 15:05:13 +0700

    Initial commit: 6 Desember 15.05

--- frontendweb/src/views/tables/ListAsset.vue
+++ frontendweb/src/views/tables/ListAsset.vue
@@ -4,7 +4,7 @@
       <div class="card">
         <div class="card-header d-flex justify-content-between">
           <div class="header-title">
-            <h4 class="card-title">List Aset</h4>
+            <h4 class="card-title">List Aset Repo</h4>
           </div>
           <div>
             <button 
